77989451084c1a503c82382f4df47d33b920cb6a
inno10 <innovale.dev@gmail.com>
2023-02-03 17:51:21 +0900

    2023.2.3 박중선 ItemRepositoryTest 상품명 조회 테스트 완료

--- src/test/java/com/shop/repository/ItemRepositoryTest.java
+++ src/test/java/com/shop/repository/ItemRepositoryTest.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -21,7 +22,7 @@ class ItemRepositoryTest {
 
     @Test
     @DisplayName("상품 저장 테스트")
-    public void createItemTest(){
+    public void createItemList(){
         Item item = new Item();
         item.setItemName("테스트 상품");
         item.setPrice(10000);
@@ -32,6 +33,16 @@ class ItemRepositoryTest {
         item.setUdtTime(LocalDateTime.now());
         Item savedItem = itemRepository.save(item);
         System.out.println(savedItem.toString());
+    }
+
+    @Test
+    @DisplayName("상품명 조회 테스트")
+    public void findByItemNameTest(){
+        this.createItemList();
+        List<Item> itemList = itemRepository.findByItemName("테스트 상품1");
+        for(Item item : itemList){
+            System.out.println(item.toString());
+        }
     }
 
 }
