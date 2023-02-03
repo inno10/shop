73a5adf763c014fda2e2e61a2f9972c4ae785c8c
inno10 <innovale.dev@gmail.com>
2023-02-03 15:33:30 +0900

    2023.2.3 박중선 item entity 매핑

--- src/main/java/com/shop/entity/Item.java
+++ src/main/java/com/shop/entity/Item.java
@@ -1,34 +1,46 @@
 package com.shop.entity;
 
 import com.shop.constant.ItemSellStatus;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
 import java.time.LocalDateTime;
 
+@Entity
+@Table(name="item")
 @Getter
 @Setter
 @ToString
 public class Item {
 
     //상품 코드
+    @Id
+    @Column(name="item_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     //상품 이름
+    @Column(nullable = false, length = 50)
     private String itemName;
 
     //가격
+    @Column(name="price", nullable = false)
     private int price;
 
     //재고 수량
+    @Column(nullable = false)
     private int stockNumber;
 
     //상품 상세 설명
+    @Lob
+    @Column(nullable = false)
     private String itemDetail;
 
     //상품 판매 상태
-    private ItemSellStatus itemSellStatus;
+    @Enumerated(EnumType.STRING)
+    private ItemSellStatus itemSellStatus;1
 
     //등록 시간
     private LocalDateTime regTime;
