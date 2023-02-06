002314bb2d1517ffa7e7ba10064a4d5817c9e4b3
inno10 <innovale.dev@gmail.com>
2023-02-06 22:19:45 +0900

    타임리프 컨트롤러 구문 추가

--- src/main/java/com/shop/controller/ThymeleafExController.java
+++ src/main/java/com/shop/controller/ThymeleafExController.java
@@ -1,17 +1,32 @@
 package com.shop.controller;
 
+import com.shop.dto.ItemDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.time.LocalDateTime;
+
 @Controller
-@RequestMapping(value="/thymeleaf")
+@RequestMapping(value = "/thymeleaf")
 public class ThymeleafExController {
 
-@GetMapping(value = "/ex01")
-public String thymeleafExample01(Model model){
-    model.addAttribute("data", "타임리프 예시");
-    return "thymeleafEx/thymeleafEx01";
+    @GetMapping(value = "/ex01")
+    public String thymeleafExample01(Model model) {
+        model.addAttribute("data", "타임리프 예시");
+        return "thymeleafEx/thymeleafEx01";
+    }
+
+    @GetMapping(value = "/ex021")
+    public String thymleafExample02(Model model) {
+        ItemDto itemDto = new ItemDto();
+        itemDto.setItemDetail("상품 상세 설명");
+        itemDto.setItemName("테스트 상품1");
+        itemDto.setPrice(10000);
+        itemDto.setRegTime(LocalDateTime.now());
+
+        model.addAttribute("itemDto",itemDto);
+        return "thymeleafEx/thymeleafEx02";
     }
 }
