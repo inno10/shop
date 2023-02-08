d305b32a6cea0700f97d1c0fcc4d4959247e418c
inno10 <innovale.dev@gmail.com>
2023-02-08 17:53:30 +0900

    타임리프 th:switch, th:case 조건문 처리 실행 완료

--- src/main/java/com/shop/controller/ThymeleafExController.java
+++ src/main/java/com/shop/controller/ThymeleafExController.java
@@ -69,4 +69,23 @@ public class ThymeleafExController {
         model.addAttribute("itemDtoList", itemDtoList);
         return "thymeleafEx/thymeleafEx04";
     }
+
+    @GetMapping(value="/ex05")
+    public String thymeleafExample05(Model model){
+        List<ItemDto> itemDtoList = new ArrayList<>();
+
+        for(int i = 1; i <= 10; i++){
+            ItemDto itemDto = new ItemDto();
+            itemDto.setItemDetail("상품 상세 설명"+ i);
+            itemDto.setItemName("테스트 상품" + i);
+            itemDto.setPrice(1000* i);
+            itemDto.setRegTime(LocalDateTime.now());
+
+            itemDtoList.add(itemDto);
+        }
+
+        model.addAttribute("itemDtoList", itemDtoList);
+        return "thymeleafEx/thymeleafEx04";
+    }
 }
+
