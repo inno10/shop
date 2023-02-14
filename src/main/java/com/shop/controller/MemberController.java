94ebce8b33124e5d6bcbada792e77904d3e8cc36
inno10 <innovale.dev@gmail.com>
2023-02-14 20:51:02 +0900

    회원가입 검증/에러 메시지 반환 메서드 추가

--- src/main/java/com/shop/controller/MemberController.java
+++ src/main/java/com/shop/controller/MemberController.java
@@ -3,10 +3,12 @@ package com.shop.controller;
 import com.shop.dto.MemberFormDto;
 import com.shop.entity.Member;
 import com.shop.service.MemberService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,11 +28,26 @@ public class MemberController {
     }
 
     @PostMapping(value = "/new")
-    public String memberForm(MemberFormDto memberformDto){
+    public String memberForm(MemberFormDto memberformDto) {
 
-        Member member= Member.createMember(memberformDto, passwordEncoder);
+        Member member = Member.createMember(memberformDto, passwordEncoder);
         memberService.saveMember(member);
 
+        return "redirect:/";
+    }
+
+    //검증 실패시 에러 메시지 받음 (검사 후 결과는 bindingResult에 담기며, 에러가 있으면 회원가입 페이지로 이동)
+    @PostMapping(value = "/new")
+    public String newMember(@Valid MemberFormDto memberFormDto, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            return "member/memberForm";
+        }
+        try {
+            Member member = Member.createMember(memberFormDto, passwordEncoder);
+        } catch (IllegalStateException e) {
+            model.addAttribute("errorMessage", e.getMessage());
+            return "member/memberForm";
+        }
         return "redirect:/";
     }
 }
