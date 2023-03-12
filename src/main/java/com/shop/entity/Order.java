4d54a83232b0d6999b39f47d837da8f6e280ce28
박중선 <innovale.dev@gmail.com>
2023-03-13 02:29:09 +0900

    고아 객체 제거 테스트 완료

--- src/main/java/com/shop/entity/Order.java
+++ src/main/java/com/shop/entity/Order.java
@@ -32,7 +32,9 @@ public class Order {
 
     /*주문 상품 엔티티와 일대다 매핑
     하나의 주문이 여러개의 주문 상품을 가지기 떄문에 List 타입으로 매핑*/
-    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    //부모 엔티티의 영속성 상태 변화를 자식 엔티티에 모두 전이 (CascadeType.ALL)
+    //고아객체 제거 (orphanRemuval=true)
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<OrderItem> orderItems = new ArrayList<>();
 
     private LocalDateTime regTime;
--- src/test/java/com/shop/entity/OrderTest.java
+++ src/test/java/com/shop/entity/OrderTest.java
@@ -2,6 +2,7 @@ package com.shop.entity;
 
 import com.shop.constant.ItemSellStatus;
 import com.shop.repository.ItemRepository;
+import com.shop.repository.MemberRepository;
 import com.shop.repository.OrderRepository;
 import jakarta.persistence.*;
 import org.junit.jupiter.api.DisplayName;
@@ -14,6 +15,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 @TestPropertySource(locations = "classpath:application-test.properties")
 @Transactional
@@ -65,8 +67,43 @@ public class OrderTest {
         //데이터 베이스에서 주문 엔티티를 조회
         Order savedOrder = orderRepository.findById(order.getId())
                 .orElseThrow(EntityExistsException::new);
-       assertEquals(3, savedOrder.getOrderItems().size());
+        assertEquals(3, savedOrder.getOrderItems().size());
 
+    }
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    //주문 데이터를 생성해서 저장
+    public Order createOrder() {
+        Order order = new Order();
+
+        for (int i = 0; i < 3; i++) {
+            Item item = createItem();
+            itemRepository.save(item);
+            OrderItem orderItem = new OrderItem();
+            orderItem.setItem(item);
+            orderItem.setCount(10);
+            orderItem.setOrderPrice(1000);
+            orderItem.setOrder(order);
+            order.getOrderItems().add(orderItem);
+        }
+
+        Member member = new Member();
+        memberRepository.save(member);
+
+        order.setMember(member);
+        orderRepository.save(order);
+        return order;
+    }
+
+    @Test
+    @DisplayName("고아객체 제거 테스트")
+    public void orphanRemovalTest() {
+        Order order = this.createOrder();
+        //order 엔티티에서 관리하고 있는 orderItem 리스트의 0번째 인덱스 요소 제거
+        order.getOrderItems().remove(0);
+        em.flush();
     }
 
 }
